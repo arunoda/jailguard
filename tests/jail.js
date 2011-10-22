7e999366606fc6686ab9dd99c4ff7cd290ec1aee
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2011-10-23 03:00:46 +0530

    added process based executor

---
+++ tests/jail.js
@@ -0,0 +1,112 @@
+var jailguard = require('jail');
+
+exports.testRunSuccess = function(test) {
+	
+	var jg = jailguard.create();
+	var js = "abc = 10 + 20;"
+	var env = {abc: null}
+	jg.run(js, env, function(err) {
+		console.log(err);
+		test.ok(!err);
+		test.equal(env.abc, 30);
+		test.done();
+	});
+};
+
+exports.testRunError = function(test) {
+	
+	var jg = jailguard.create();
+	var js = "abc 34 34 10 + 20;" // error code
+	var env = {abc: null}
+	jg.run(js, env, function(err) {
+		test.ok(err);
+		test.equal(env.abc, null);
+		test.done();
+	});
+};
+ 
+exports.testRunNeverEndingLoopDetect = function(test) {
+	
+	var jg = jailguard.create({timeout: 100});
+	var js = "var aa = 10; while(true) { abc=++aa;} \n ccd=200;";
+	var env = {abc: 10, ccd: null}
+	jg.run(js, env, function(err) {
+		test.ok(err);
+		test.equal(err.code, 'NEVER_ENDING_LOOP');
+		test.done();
+	});
+
+};
+
+exports.testRunNormalLoopDetect = function(test) {
+	
+	var jg = jailguard.create({timeout: 100});
+	var js = "var aa = true; while(aa) { aa= false;} \n ccd=200;";
+	var env = {abc: 10, ccd: null}
+	jg.run(js, env, function(err) {
+		test.ok(!err);
+		test.done();
+	});
+
+};
+
+exports.testRunNeverEndingForLoopDetect = function(test) {
+	
+	var jg = jailguard.create({timeout: 100});
+	var js = "var aa = 10; for(var aa=0; true; aa++) { abc=++aa;} \n ccd=200;";
+	var env = {abc: 10, ccd: null}
+	jg.run(js, env, function(err) {
+		test.ok(err);
+		test.equal(err.code, 'NEVER_ENDING_LOOP');
+		test.done();
+	});
+
+};
+
+exports.testRunNormalForLoopDetect = function(test) {
+	
+	var jg = jailguard.create({timeout: 100});
+	var js = "var aa = 10; for(var aa=0; aa <10 ; aa++) { abc=++aa;} \n ccd=200;";
+	var env = {abc: 10, ccd: null}
+	jg.run(js, env, function(err) {
+		test.ok(!err);
+		test.done();
+	});
+
+};
+
+exports.testNoEval = function(test) {
+	
+	var jg = jailguard.create({timeout: 100});
+	var js = "var aa = 10; eval('aa=100;') ";
+	var env = {aa: 0, ccd: null}
+	jg.run(js, env, function(err) {
+		test.ok(err);
+		test.equal(err.code, 'EVAL_NOT_SUPPORTED');
+		test.done();
+	});
+};
+
+exports.testNoSetTimeout = function(test) {
+	
+	var jg = jailguard.create({timeout: 100});
+	var js = "var aa = 10; setTimeout(function() {aa=100}, 0); ";
+	var env = {aa: 0, ccd: null}
+	jg.run(js, env, function(err) {
+		test.ok(err);
+		test.equal(err.code, 'SETTIMEOUT_NOT_SUPPORTED');
+		test.done();
+	});
+};
+
+exports.testNoSetInterval = function(test) {
+	
+	var jg = jailguard.create({timeout: 100});
+	var js = "var aa = 10; setInterval(function() {aa=100}, 0); ";
+	var env = {aa: 0, ccd: null}
+	jg.run(js, env, function(err) {
+		test.ok(err);
+		test.equal(err.code, 'SETINTERVAL_NOT_SUPPORTED');
+		test.done();
+	});
+};
--- tests/jailguard.js
+++ tests/jailguard.js
@@ -1,112 +1,15 @@
 var jailguard = require('jailguard');
 
-exports.testRunSuccess = function(test) {
+
+exports.testNormal = function(test) {
 	
 	var jg = jailguard.create();
-	var js = "abc = 10 + 20;"
-	var env = {abc: null}
-	jg.run(js, env, function(err) {
-		console.log(err);
+	var env = {aa: 0};
+	var code = "aa = 100;"
+	jg.run(code, env, function(err) {
+		
 		test.ok(!err);
-		test.equal(env.abc, 30);
-		test.done();
-	});
-};
-
-exports.testRunError = function(test) {
-	
-	var jg = jailguard.create();
-	var js = "abc 34 34 10 + 20;" // error code
-	var env = {abc: null}
-	jg.run(js, env, function(err) {
-		test.ok(err);
-		test.equal(env.abc, null);
-		test.done();
-	});
-};
- 
-exports.testRunNeverEndingLoopDetect = function(test) {
-	
-	var jg = jailguard.create({timeout: 100});
-	var js = "var aa = 10; while(true) { abc=++aa;} \n ccd=200;";
-	var env = {abc: 10, ccd: null}
-	jg.run(js, env, function(err) {
-		test.ok(err);
-		test.equal(err.code, 'NEVER_ENDING_LOOP');
-		test.done();
-	});
-
-};
-
-exports.testRunNormalLoopDetect = function(test) {
-	
-	var jg = jailguard.create({timeout: 100});
-	var js = "var aa = true; while(aa) { aa= false;} \n ccd=200;";
-	var env = {abc: 10, ccd: null}
-	jg.run(js, env, function(err) {
-		test.ok(!err);
-		test.done();
-	});
-
-};
-
-exports.testRunNeverEndingForLoopDetect = function(test) {
-	
-	var jg = jailguard.create({timeout: 100});
-	var js = "var aa = 10; for(var aa=0; true; aa++) { abc=++aa;} \n ccd=200;";
-	var env = {abc: 10, ccd: null}
-	jg.run(js, env, function(err) {
-		test.ok(err);
-		test.equal(err.code, 'NEVER_ENDING_LOOP');
-		test.done();
-	});
-
-};
-
-exports.testRunNormalForLoopDetect = function(test) {
-	
-	var jg = jailguard.create({timeout: 100});
-	var js = "var aa = 10; for(var aa=0; aa <10 ; aa++) { abc=++aa;} \n ccd=200;";
-	var env = {abc: 10, ccd: null}
-	jg.run(js, env, function(err) {
-		test.ok(!err);
-		test.done();
-	});
-
-};
-
-exports.testNoEval = function(test) {
-	
-	var jg = jailguard.create({timeout: 100});
-	var js = "var aa = 10; eval('aa=100;') ";
-	var env = {aa: 0, ccd: null}
-	jg.run(js, env, function(err) {
-		test.ok(err);
-		test.equal(err.code, 'EVAL_NOT_SUPPORTED');
-		test.done();
-	});
-};
-
-exports.testNoSetTimeout = function(test) {
-	
-	var jg = jailguard.create({timeout: 100});
-	var js = "var aa = 10; setTimeout(function() {aa=100}, 0); ";
-	var env = {aa: 0, ccd: null}
-	jg.run(js, env, function(err) {
-		test.ok(err);
-		test.equal(err.code, 'SETTIMEOUT_NOT_SUPPORTED');
-		test.done();
-	});
-};
-
-exports.testNoSetInterval = function(test) {
-	
-	var jg = jailguard.create({timeout: 100});
-	var js = "var aa = 10; setInterval(function() {aa=100}, 0); ";
-	var env = {aa: 0, ccd: null}
-	jg.run(js, env, function(err) {
-		test.ok(err);
-		test.equal(err.code, 'SETINTERVAL_NOT_SUPPORTED');
+		test.equal(100, env.aa);
 		test.done();
 	});
 };
