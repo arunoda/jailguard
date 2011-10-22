3ff360016374dd919140185ec2ed3fabffffa883
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2011-10-23 03:40:30 +0530

    stabilized process based executor

--- tests/jailguard.js
+++ tests/jailguard.js
@@ -13,3 +13,21 @@ exports.testNormal = function(test) {
 		test.done();
 	});
 };
+
+exports.testKillAndRestart = function(test) {
+	
+	var jg = jailguard.create();
+	jg._child.kill('SIGQUIT');
+	
+	
+	setTimeout(function() {
+		
+		var env = {aa: 0};
+		var code = "aa = 100;"
+		jg.run(code, env, function(err) {
+			test.ok(!err);
+			test.equal(100, env.aa);
+			test.done();
+		});
+	}, 10);
+};
